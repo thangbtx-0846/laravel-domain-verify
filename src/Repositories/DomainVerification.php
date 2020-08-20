5bf01bd0f958413e4db04cfd07ef4f2d1876c407
tranxuanthang <tran.xuan.thangb@sun-asterisk.com>
2020-08-20 10:53:22 +0700

    fix: polymorphic

--- src/Repositories/DomainVerification.php
+++ src/Repositories/DomainVerification.php
@@ -40,10 +40,8 @@ class DomainVerification implements DomainVerificationInterface
 
     public function firstOrCreate(string $url, DomainVerifiableInterface $verifiable): DomainVerificationModel
     {
-        return DomainVerificationModel::firstOrCreate(
+        return $verifiable->domainVerifications()->firstOrCreate(
             [
-                'verifiable_type' => get_class($verifiable),
-                'verifiable_id' => $verifiable->getKey(),
                 'url' => $url,
             ],
             [
